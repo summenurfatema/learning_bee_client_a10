8bd0e038a85168a258376945218840ae94736876
Fatema04 <shadrinmoni15@gmail.com>
2022-10-25 08:04:20 -0700

    google login added

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,19 +1,30 @@
 
-import { useLoaderData } from 'react-router-dom';
+
+import { useState, useEffect } from 'react';
 import Course from '../Course/Course';
+import { Link } from 'react-router-dom';
 
 const Courses = () => {
-    const courses = useLoaderData()
+    const [allCourse, setAllCourse] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/course-info')
+            .then(res => res.json())
+            .then(data => setAllCourse(data))
+    }, [])
 
     return (
         <div className='flex justify-between'>
             <div className='pl-12'>
                 <h2 className='pb-2 border-b-2 mb-5 font-bold text-2xl'>Our Courses</h2>
                 {
-                    courses.map(course => <p className='text-xl font-semibold'
-                        key={course.id}>{course.name}
+                    allCourse.map(course => <p key={course.id}>
+                        <Link className='text-xl font-semibold text-gray-800' to={`/course-info/${course.id}`}>{course.name}</Link>
+
                     </p>)
+
                 }
+
+
             </div>
             <div><Course></Course></div>
         </div>
